e175a05198f88845dabd57c0688a749fe2ef2d8d
Awish Dot <awishdotid@gmail.com>
2024-11-07 06:42:00 +0800

    buat gitignore

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  /* config options here */
+  // basePath: "/pasarbone",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+}
+ 
+export default nextConfig

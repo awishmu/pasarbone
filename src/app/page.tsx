d9226a8651b4fd36a2a4659fcedcbcc24a354f72
Awish Mu <61050376+awishmu@users.noreply.github.com>
2024-11-07 15:50:31 +0800

    Update page.tsx
    bg-white bg-sky-200

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
   <>
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-sky-200 dark:bg-gray-900">
 		    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
 		        <div className="mr-auto place-self-center lg:col-span-7">
 		            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>

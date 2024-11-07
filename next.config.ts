c03b9957dac285a0c6fb9214eac9f55a00f5ddaa
Awish Mu <61050376+awishmu@users.noreply.github.com>
2024-11-07 08:10:58 +0800

    Update next.config.ts
    assetprefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/pasarbone",
+ assetPrefix: "/pasarbone",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 }

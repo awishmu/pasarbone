db9df0fffa874b9cfc67491e85ad2272b670752c
Bone4Coder <bonekecamatancina@gmail.com>
2024-11-12 20:56:15 +0800

    kerja area item

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	images: {
-		domains: ['images.unsplash.com', 'media.istockphoto.com'],
+		domains: ['images.unsplash.com', 'media.istockphoto.com', 'cdn0-production-images-kly.akamaized.net'],
 	}, 
 }
--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -6,22 +6,30 @@ export default function Area() {
   return (
 	<>
 		<div>
-			<section>
-				<div className="flex flex-col">
-					<span>Product</span>
-					<h2>Our Hot Product</h2>
+			<section className=" ">
+				<div className="header w-full mx-auto flex flex-col justify-center">
+					<span className=" mx-auto">Product</span>
+					<h2 className="mx-auto">Our Hot Product</h2>
 				</div>
-				<div className="">
-					<Link href="#">
-						<div className="image">
-							Image />
-						</div>
-						<div className="content">
-							<h3>Honey</h3>
-							<span>$22.20</span>
-							<button>Add</button>
-						</div>
-					</Link>>
+				<div className="wrapper w-full">
+					<div className="wrap-item border-lime-500 border-2 max-w-[275px] bg-sky-700">
+						<Link href="#">
+							<div className="image">
+								<Image 
+									src="https://cdn0-production-images-kly.akamaized.net/IwibSg0P6WOSFCLqi6frpxnhwSA=/0x24:999x587/680x383/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3420473/original/044747700_1617644743-shutterstock_1064990636.jpg" 
+									alt="kripik" 
+									width={100} height={80}
+									fill={}
+								/>
+							</div>
+							<div className="content">
+								<h3>Honey</h3>
+								<span>$22.20</span>
+								<button>Add</button>
+							</div>
+						</Link>
+					</div>
+					
 
 
 				</div>

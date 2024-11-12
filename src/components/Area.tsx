cb2f50651090289e0ee05ba2b93936f77c81a530
Bone4Coder <bonekecamatancina@gmail.com>
2024-11-12 21:31:11 +0800

    kerja area item

--- src/components/Area.tsx
+++ src/components/Area.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link"; 
 
-
 export default function Area() {
   return (
 	<>
@@ -19,7 +18,7 @@ export default function Area() {
 									src="https://cdn0-production-images-kly.akamaized.net/IwibSg0P6WOSFCLqi6frpxnhwSA=/0x24:999x587/680x383/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3420473/original/044747700_1617644743-shutterstock_1064990636.jpg" 
 									alt="kripik" 
 									width={100} height={80}
-									fill={}
+									
 								/>
 							</div>
 							<div className="content">
@@ -29,13 +28,9 @@ export default function Area() {
 							</div>
 						</Link>
 					</div>
-					
-
-
 				</div>
 			</section>
 		</div>
-		
 	</>
-  );
-}
+  )
+};

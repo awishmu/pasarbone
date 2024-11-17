f44c26da8655c4293f51f6f8436d29c778467fa4
Awish Dot <awishdotid@gmail.com>
2024-11-17 22:58:18 +0800

    perbaiki navbar v1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { House } from 'lucide-react';
 import { Search } from 'lucide-react';
 import { Menu } from 'lucide-react';
+import PBCLogo from '/public/pbc_logo.png'
 
 const NavLinks = [
 	{
@@ -31,14 +32,14 @@ export default function Navbar() {
 	<>
 		<nav className="bg-gray-900 shadow-sm w-full">
 			<div className="container mx-auto flex justify-between" >
-				<div className="relative w-full inline-block">
-					<div className="w-full">
-							<div className="flex shrink-0 items-center ">
-								<i><House /></i>
+				<div className="w-full dark:text-gray-100 text-gray-100">
+					<div className="relative w-full flex justify-between items-center">
+							<div className="flex shrink-0 items-center py-4 ">
+								<Link href="#"><Image src={PBCLogo} alt={'PBCLogo'} width={50} height={50} /></Link>
 							</div>
 						<div className="nav_main flex flex-1 items-center justify-center md:justify-between">
-							<div className="nav_right flex flex-1 items-center justify-center sm:items-stretch sm:justify-start ">
-								<ul className="flex md:inline-block sm:ml-6 ">
+							<div className="nav_right hidden lg:flex  flex-1 items-center justify-center sm:items-stretch sm:justify-start ">
+								<ul className="flex sm:ml-6 ">
 										<li className="py-6 px-4 hover:bg-gray-700 flex gap-2">
 											<i><House /></i>Home Page
 										</li>
@@ -49,7 +50,7 @@ export default function Navbar() {
 									))}
 								</ul>
 							</div>
-							<div className="nav_left">
+							<div className="nav_left mx-auto">
 								<form>	
 									<div className="flex items-center py-6 px-4 gap-2">
 										<input className="" type="text" placeholder="...search" name="search" />
@@ -58,12 +59,14 @@ export default function Navbar() {
 								</form>
 							</div>
 						</div>
+						<div className="absolute  lg:hidden z-20 right-0">
+							{/*  Mobile menu button */}
+							<button><i><Menu /></i></button>
+						</div>
+						
 					</div>
 				</div>
-				<div className="absolute inline md:hidden">
-					{/*  Mobile menu button */}
-					<button><i><Menu /></i></button>
-				</div>
+				
 			</div>
 		
 			{/* <!-- Mobile menu, show/hide based on menu state. --> */}
